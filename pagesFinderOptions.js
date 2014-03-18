82f73b68711b9b956b7d6b7c22fc90eb1d20b14c
Francisco Baio Dias <xicombd@gmail.com>
2014-03-18 09:23:26 +0000

    find pages

--- pagesFinderOptions.js
+++ pagesFinderOptions.js
@@ -1,16 +1,18 @@
 var options = {
   cities: [
     {name: 'Lisboa'},
-    //{name: 'Porto'},
-    //{name: 'Aveiro'},
-    //{name: 'Leiria'},
-    //{name: 'Coimbra'}
+    {name: 'Porto'},
+    {name: 'Aveiro'},
+    {name: 'Leiria'},
+    {name: 'Coimbra'}
   ],
   queries: [
     {q: 'estudantes'},
     {q: 'universidade'},
     {q: 'faculdade'},
-    {q: 'praxe'}
+    {q: 'praxe'},
+    {q: 'bar'},
+    {q: 'discoteca'}
     //{q: 'grupo+estudantes'},
     //{q: 'associação+estudantes'},
     //{q: 'núcleo+estudantes'}
