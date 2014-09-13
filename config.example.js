044e40a80f8fae58e54034bd165d84e499f301d9
Francisco Baio Dias <xicombd@gmail.com>
2014-09-13 22:07:37 +0100

    Added config example file

---
+++ config.example.js
@@ -0,0 +1,20 @@
+var config = {
+  url: process.env.EVENTHUB_URL || 'http://localhost:8080',
+  port: process.env.EVENTHUB_PORT || 8080,
+  updateOnStart: process.env.EVENTHUB_UPDATE_ON_START || false
+};
+
+config.mongo = {
+  url: process.env.EVENTDECK_MONGO_URL || 'mongodb://localhost/eventhub'
+};
+
+config.facebook = {
+  accessToken : process.env.EVENTHUB_FACEBOOK_TOKEN || 'YOUR ACCESS TOKEN'
+};
+
+config.bunyan = {
+  name: require('./package.json').name,
+  level: process.env.EVENTHUB_LOG_LEVEL || 'trace'
+};
+
+module.exports = config;
